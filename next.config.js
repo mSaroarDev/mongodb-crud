5168b449a93630c85081a7b454e710217268f3ee
unknown <msaroar.dev@gmail.com>
2024-03-06 00:08:48 +0600

    next config updated

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET,DELETE,PATCH,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
@@ -23,4 +23,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
